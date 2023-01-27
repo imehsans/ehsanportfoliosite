cc53a8c385e09890fc66be3dba5d5f6854457c08
imehsans <mehsanjadoon786@gmail.com>
2023-01-27 15:08:37 -0500

    ehsanportfolio site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Blogs from './components/blogs/Blogs'
+import Contact from './components/contact/Contact'
+import Home from './components/home/Home'
+import Layout from './components/Layout/Layout'
+import NoPage from './components/noPage/NoPage'
+import Projects from './components/projects/Projects'
+import Skills from './components/skills/Skills'
+import Work from './components/work/Work'
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+					<Route path="work" element={<Work />} />
+					<Route path="projects" element={<Projects />} />
+					<Route path="blogs" element={<Blogs />} />
+					<Route path="skills" element={<Skills />} />
+					<Route path="contact" element={<Contact />} />
+					<Route path="*" element={<NoPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
-export default App;
+export default App
